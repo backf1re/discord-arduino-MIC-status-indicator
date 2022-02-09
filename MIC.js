b365992d8a13907d8c17235d5ac55b1d22082b65
backf1re <33880547+backf1re@users.noreply.github.com>
2022-02-10 02:15:13 +0300

    renamed script name, made some tweaks
    added definition of arduino port number
    changed led pin number

--- MIC.js
+++ MIC.js
@@ -1,17 +1,19 @@
 const Discord = require('discord.js');
 const five = require('johnny-five');
 const client = new Discord.Client();
-const board = new five.Board();
+const board = new five.Board({
+  port: "COM13" // change to port where your arduino connected
+});
 board.on("ready", () => {
 	const led = new five.Led(2);
 	client.on('voiceStateUpdate', (oldState, newState) => { 
-		if (oldState.member.user.id !== "youridhere") return // make sure its only checking for your microhpone
+		if (oldState.member.user.id !== "monitored-user-id") return // make sure its only checking for your microhpone
 		if (newState.mute) { // check if mic is muted
 		led.on() //if true make light on
 		} else {	
 		led.off() //if false make light off
-	})
-})
-})
+	}
+});
+});
 
-client.login('YOUR BOT TOKEN HERE'); // https://discordapp.com/developers/applications/
+client.login('your-bot-id');
