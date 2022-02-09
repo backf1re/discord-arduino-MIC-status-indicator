88a6727d8c296595d05e4f42ba465a471dff9421
backf1re <33880547+backf1re@users.noreply.github.com>
2022-02-09 17:58:43 +0300

    integrated mic state code

--- server.js
+++ server.js
@@ -4,19 +4,14 @@ const client = new Discord.Client();
 const board = new five.Board();
 board.on("ready", () => {
 	const led = new five.Led(13);
-	client.on('message', msg => { 
-		if (msg.content === "led on") led.on()
-		else if (msg.content === "led off") led.off()
-	});
-});
-//this part is for mic state led
-//client.on('voiceStateUpdate', (oldState, newState) => {
-//  if (oldState.member.user.id !== "youridhere") return // make sure its only checking for your microhpone
-//  if (newState.mute) { // check if mic is muted
-//    setLightColor("red") //if true make light red
-//  } else {
-//    setLightColor("green") //if false make light green
-//  }
-//})
+	client.on('voiceStateUpdate', (oldState, newState) => { 
+		if (oldState.member.user.id !== "youridhere") return // make sure its only checking for your microhpone
+		if (newState.mute) { // check if mic is muted
+		led.on() //if true make light on
+		} else {	
+		led.off() //if false make light off
+	})
+})
+})
 
 client.login('YOUR BOT TOKEN HERE'); // https://discordapp.com/developers/applications/
