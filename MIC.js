a746187daa258213390931669f438fc55bdbee9a
backf1re <33880547+backf1re@users.noreply.github.com>
2022-02-10 11:33:59 +0300

    final polishing
    made id check work correct

--- MIC.js
+++ MIC.js
@@ -2,13 +2,14 @@ const Discord = require('discord.js');
 const five = require('johnny-five');
 const client = new Discord.Client();
 const board = new five.Board({
-  port: "COM13" // change to port where your arduino connected
+  port: "COM13" //change to your arduino com port
 });
 board.on("ready", () => {
 	const led = new five.Led(2);
 	client.on('voiceStateUpdate', (oldState, newState) => { 
-		if (oldState.member.user.id !== "monitored-user-id") return // make sure its only checking for your microhpone
+		if (oldState.user.id !== "your discord id here") return // make sure its only checking for your (id) icrohpone
 		if (newState.mute) { // check if mic is muted
+		//console.log(oldState.user.tag);//this string is debug to check if it check your id correctly
 		led.on() //if true make light on
 		} else {	
 		led.off() //if false make light off
@@ -16,4 +17,4 @@ board.on("ready", () => {
 });
 });
 
-client.login('your-bot-id');
+client.login('bot id here');
